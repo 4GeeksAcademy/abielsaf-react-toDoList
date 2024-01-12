e8f2dc5643e3b1aff99aae71d5d50c08218c56ca
Ángel Bielsa <145146806+abielsaf@users.noreply.github.com>
2024-01-12 20:06:34 +0000

    Add and delete working, validation and style missing

---
+++ src/js/component/TodoList.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+
+function TodoList () {
+  const [todos, setTodos] = useState([])
+  const [inputValue, setInputValue] = useState('')
+
+function handleChange(e){
+  setInputValue(e.target.value)
+}
+
+function handleSubmit(e){
+  e.preventDefault()
+  setTodos([...todos, inputValue])
+  setInputValue('')
+}
+
+function handleDelete(index){
+  const newTodos = [...todos]
+  newTodos.splice(index, 1)
+  setTodos(newTodos)
+}
+  return (
+    <div className='text-center'>
+      <h1>To do List</h1>
+      <br></br>
+      <form>
+        <input type='text' value={inputValue} onChange={handleChange}/>
+        <button className="add-button" onClick={handleSubmit}>Add to do!</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}
+          <button className="delete-button" onClick={() =>handleDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <p> Tasks left: {todos.length} </p>
+    </div>
+  )
+}
+
+export default TodoList;
+
