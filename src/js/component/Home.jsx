e5d6dee220844fb63f55e12f33f816d4f6c54e74
Ángel Bielsa <145146806+abielsaf@users.noreply.github.com>
2024-01-15 17:56:22 +0000

    Created home component

---
+++ src/js/component/Home.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import TodoList from "./TodoList.jsx";
+
+//create your first component
+		const Home = () => {
+		return (<TodoList />
+		
+		
+	);
+};
+
+export default Home
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import TodoList from "./component/TodoList.jsx";
+import Home from "./component/Home.jsx";
 
 //render your react application
-ReactDOM.render(<TodoList />, document.querySelector("#app"));
+ReactDOM.render(<Home/>, document.querySelector("#app"));
